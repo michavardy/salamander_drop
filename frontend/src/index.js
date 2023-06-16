1cb270ff2b7cecf179774bb1280fcd069f50d97a
micha <micha.vardy@tufin.com>
2023-06-16 08:28:19 +0300

    fixed css problem in production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,4 +9,4 @@ root.render(
 );
 // hot reloading. It works by replacing a module of the application
 // during runtime with an updated one so that it’s available for instant use.
-module.hot.accept();
+//module.hot.accept();
